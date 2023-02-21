a9ea16a8fc1c5e776bb1595887ed9079388dbf13
shambuwu <balfappius@protonmail.ch>
2023-02-21 12:08:26 +0100

    Add React to monolith*

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\React\ReactBundle::class => ['all' => true],
 ];
