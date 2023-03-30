8d9674867c0a271dd9bc8d5be51789677fa0dfea
shambuwu <balfappius@protonmail.ch>
2023-03-30 19:59:44 +0200

    Finalize authentication

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,7 +50,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('administration@iwa.com', 'IWA Mail Bot'))
+                    ->from(new Address('iwa.gaming@outlook.com', 'IWA Mail Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -80,7 +80,7 @@ class RegistrationController extends AbstractController
         } catch (VerifyEmailExceptionInterface $exception) {
             $this->addFlash('verify_email_error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('index');
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
